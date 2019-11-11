ddeecb6c9b7177c30d4cfe86ee7065aeddbfbedf
Anselm Joseph <anselmjosephs@gmail.com>
2019-11-11 12:40:52 +0530

    fix no data template

--- dist/table-moderno.js
+++ dist/table-moderno.js
@@ -861,7 +861,8 @@ class TableModerno {
 
   showNoDataAvailable() {
     if (this.tableData.length > 0) return false;
-    $(`#${this.tableID}`).html(this.config.noDataTemplate);
+    if ($('moderno-table-no-data').length > 0) return true;
+    $(`#${this.tableID}`).append(this.config.noDataTemplate);
     $(`#${this.tableID} .moderno-table-no-data`).css({ 'width':  $(`#${this.tableID}`).width() - 10 })
     return true;
   }
